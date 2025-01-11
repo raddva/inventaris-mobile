ac37f904003b298b6e5850439ace2d934f1c58e2
sheila dellprecision <sheiladsyandana@gmail.com>
2025-01-12 03:03:50 +0700

    add

---
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:inventaris/Screens/Welcome/components/background.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(imp),
+    );
+  }
+}
